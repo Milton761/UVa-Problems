404b7ec6b30c34257ac0be1788266f51efe9ff90
Milton <milton761@gmail.com>
2023-06-25 01:17:27 -0500

    01124

---
+++ 01124.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <string>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+
+    string equation;
+    while(getline(cin, equation)) {
+        cout<<equation<<endl;
+    }
+
+    return 0;
+}
